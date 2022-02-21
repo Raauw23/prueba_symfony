edfb94663911401f9a6bf4d6247c35d180a96744
Raul Garcia <rgarciapulido24@gmail.com>
2022-02-21 14:43:56 +0100

    Cambios

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(){
+    public function homepage(Environment $twigEnviroment){
+
+//        $html=$twigEnviroment->render('question/homepage.html.twig');
+//
+//        return new Response($html);
+
         return $this->render('question/homepage.html.twig');
 //        return new Response('What a bewitching controller we have conjured!');
     }
